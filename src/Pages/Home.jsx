706e2dfe5c079e1d6ebd8349683eb33df00f5365
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-03-26 10:33:10 +0600

    banner section create

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Carousels from './Banner/Carousels';
 
 const Home = () => {
     return (
         <div>
-       
+            <Carousels></Carousels>
         </div>
     );
 };
