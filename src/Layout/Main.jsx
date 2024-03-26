1e32cf206260dfd8e88d60714aefaad4083cdb78
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-03-26 09:28:06 +0600

    footer section create

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Home from '../Pages/Home';
+
+const Main = () => {
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Main;
