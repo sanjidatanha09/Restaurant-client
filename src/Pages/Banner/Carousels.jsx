f8de6328b57097f5590fac3691ad0dab402af10e
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-03-26 10:52:17 +0600

    banner section add new image

--- src/Pages/Banner/Carousels.jsx
+++ src/Pages/Banner/Carousels.jsx
@@ -2,23 +2,45 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import img1 from '../../assets/Carousels-img/Carousel-1.avif';
+
+import img2 from '../../assets/Carousels-img/Carousel-2.jpg';
+
+import img3 from '../../assets/Carousels-img/Carousel-3.jpg';
+import img4 from '../../assets/Carousels-img/Carousel-4.jpg';
+import img5 from '../../assets/Carousels-img/Carousel-5.jpg';
+import img6 from '../../assets/Carousels-img/Carousel-6.jpg';
+
 
 const Carousels = () => {
     return (
-        <div>
-            <Carousel>
-                <div>
-                    <img src="assets/1.jpeg" />
-                    <p className="legend">Legend 1</p>
+        <div className=''>
+            <Carousel className=' h-[1000px]'>
+                <div className='h-[700px] '>
+                    <img className='h-full' src={img1} />
+                    <p className="legend">Place 1</p>
                 </div>
-                <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
+                <div className='h-[500px] '>
+                    <img className='h-full' src={img2} />
+                    <p className="legend">Place 2</p>
                 </div>
-                <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
+                <div className='h-[500px] '>
+                    <img className='h-full'  src={img3} />
+                    <p className="legend">Place 3</p>
                 </div>
+                <div className='h-[500px] '>
+                    <img className='h-full' src={img4} />
+                    <p className="legend">Place 4</p>
+                </div>
+                <div className='h-[500px] '>
+                    <img className='h-full' src={img5} />
+                    <p className="legend">Place 5</p>
+                </div>
+                <div className='h-[500px] '>
+                    <img className='h-full' src={img6} />
+                    <p className="legend">Place 6</p>
+                </div>
+               
             </Carousel>
         </div>
     );
